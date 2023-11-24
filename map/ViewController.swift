59b2ee8e18b0e0a5bf695a9af083352aa9f458ca
Evgeny <florans1807@mail.ru>
2023-11-24 16:00:27 +0300

    Initial Commit

---
+++ map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  map
+//
+//  Created by Флоранс on 24.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
